b242f6713c42bbf138158c8adecb92fbe64a78d6
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-11-25 19:03:28 +0545

    remove jwt from upload audio

--- BolaNepal-Backend/src/routes/audio.routes.js
+++ BolaNepal-Backend/src/routes/audio.routes.js
@@ -8,13 +8,11 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router
-  .route("/upload")
-  .post(
-    verifyJWT,
-    upload.fields([{ name: "audio", maxCount: 1 }]),
-    uploadAudio
-  );
+router.route("/upload").post(
+  // verifyJWT,
+  upload.fields([{ name: "audio", maxCount: 1 }]),
+  uploadAudio
+);
 
 router.route("/getaudios").get(getAllAudioUrls);
 
